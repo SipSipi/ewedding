78c6df1e623b29ac7686cc1a80225e82ea82848b
Asif Azamuddin <105183445+SipSipi@users.noreply.github.com>
2023-04-29 05:04:16 +0800

    change sikit

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,10 +115,10 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2015,
+                year: 2023,
                 month: 6,
-                day: 28,
-                hours: 0,
+                day: 3,
+                hours: 11,
                 minutes: 0,
                 seconds: 0,
                 words: {
